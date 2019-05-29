144df8e151a762ea37038568fc5d0881ad62c4fc
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-05-29 00:04:56 -0400

    adding friends array in friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+
+let friendsArr = [
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+},
+{
+    "name":"Jeremiah",
+    "photo":"https://i.dailymail.co.uk/1s/2019/05/27/18/14023274-7075525-image-a-55_1558978808411.jpg",
+    "scores":[2,1,4,4,1,1,6,5,4,4]
+},
+{
+    "name":"Pancho",
+    "photo":"https://www.notrecinema.com/images/usercontent/star/pancho-demmings-photo_66981_29140.jpg",
+    "scores":[8,1,2,5,3,4,2,1,4,4]
+},
+{
+    "name":"Jezzika",
+    "photo":"https://img.huffingtonpost.com/asset/599f1e6f1e00001a00c5f2cf.jpg",
+    "scores":[5,3,4,4,5,4,2,5,4,1]
+}
+];
